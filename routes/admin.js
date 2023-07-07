e27f50e13fae826fcb4de0f8cc94bc3897cc8a31
Nikita Khomenko <nikita.khomenko.n25@gmail.com>
2023-07-07 14:51:45 +0300

    hotfixes/добавление полей валидации в Joi

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,7 @@
 const adminRouter = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
+const NotFoundError = require('../errors/not-found-error');
+
 const {
   createUser, login,
 } = require('../controllers/users');
@@ -8,14 +10,22 @@ adminRouter.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
+
   }),
 }), login);
 
 adminRouter.post('/signup', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/https?:\/\/[0-9a-zA-Z-._~:/?#[\]@!$&'()*+,;=]+#?/),
   }),
 }), createUser);
+
+adminRouter.use((req, res, next) => {
+  next(new NotFoundError('Страница не найдена'));
+});
 
 module.exports = adminRouter;
